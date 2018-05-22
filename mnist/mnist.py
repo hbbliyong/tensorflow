a71bfd8b760f9b0e2fba4c2604e4640be52f593a
hbbliyong <hbbliyiong@163.com>
2018-05-22 21:33:26 +0800

    修改了mnist输出

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -4,16 +4,17 @@ import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.placeholder(tf.float32, [None, 784])#28*28个点
+y_= tf.placeholder("float", [None, 10])
 
 W = tf.Variable(tf.zeros([784, 10]))
 b=tf.Variable(tf.zeros([10]))
 y=tf.nn.softmax(tf.matmul(x, W)+b)
 
-y_=tf.placeholder("float", [None, 10])
+
 cross_entropy=-tf.reduce_sum(y_*tf.log(y))
 train_step=tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init=tf.initialize_all_variables()
+init=tf.global_variables_initializer()
 
 sess=tf.Session()
 sess.run(init)
--- tensorflow/莫凡/addlayer.py
+++ tensorflow/莫凡/addlayer.py
@@ -42,7 +42,17 @@ ax=fig.add_subplot(1,1,1)
 ax.scatter(x_data,y_data)
 plt.show()
 
-for i in range(10000):
+for i in range(1000):
+    #training
     sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-    if(i%500==0):
-        print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+    if(i%50==0):
+        # to visualize the result and improvement
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value=sess.run(prediction,feed_dict={xs:x_data}) 
+        # plot the prediction
+        lines=ax.plot(x_data,prediction_value,'r-',lw=5)
+        plt.pause(0.1)   
+        #print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
